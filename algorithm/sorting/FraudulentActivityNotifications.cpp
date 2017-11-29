f349f2b519557d9ed33eed362e255e1fef46d9e9
Taylor Price <taylor.price@gmail.com>
2017-11-28 16:22:07 -0800

    Initial try. Not working yet

--- algorithm/sorting/FraudulentActivityNotifications.cpp
+++ algorithm/sorting/FraudulentActivityNotifications.cpp
@@ -3,7 +3,46 @@
 using namespace std;
 
 int activityNotifications(vector <int> expenditure, int d) {
-    // Complete this function
+    // If we don't have at least d + 1 days of expenditures,
+    // return 0 because there can't be any notifications.
+    if (expenditure.size() <= d)
+      return 0;
+
+    // For each expenditure starting at d + 1, determine if we've got a notification.
+    int notifications = 0;
+    int medianItem = (expenditure.size() - 1) / 2;
+    bool useOnlyMedianItem = d % 2;
+    for (int i = d; i < expenditure.size(); d++) {
+        multiset<int> currentTransactions;
+        
+        for (int j = 1; j <= d; j++) { 
+          currentTransactions.emplace(expenditure[i-j]);
+        }
+
+        int medianValue = 0;
+        multiset<int>::iterator it = currentTransactions.begin();
+
+        for (int j = 0; j < medianItem; j++) {
+          it++;
+        }
+        if (useOnlyMedianItem) {
+          ++it;
+          medianValue = *it;
+        }
+        else {
+          medianValue += *it;
+
+          ++it;
+          medianValue += *it;
+
+          medianValue /= 2;
+        }
+
+        if (expenditure[i] >= (medianValue * 2))
+          notifications++;
+    }
+
+    return notifications;
 }
 
 int main() {
